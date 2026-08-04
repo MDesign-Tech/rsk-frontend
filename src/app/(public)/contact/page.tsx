d0e29cc475d15c2077cd21810a9b550c4dc6a41e
Mussa Nsabimana <nsamussa1@gmail.com>
2026-08-04 21:06:43 +0200

    a

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -10,18 +10,8 @@ export const metadata = {
 export default function ContactPage() {
   return (
     <main className="relative z-0 h-max bg-background overflow-x-hidden">
-      <div
-        className="absolute top-0 right-0 w-[1500px] h-[1500px] -z-10 bg-primary pointer-events-none"
-        style={{
-          maskImage:
-            "radial-gradient(ellipse 50% 50% at 100% 0%, rgb(0 0 0 / 0.75), transparent)",
-        }}
-      >
-        <div
-          className="absolute inset-0 bg-cover bg-right-top"
-          style={{ backgroundImage: "url('/grade.png')" }}
-        />
-      </div>
+     
+        
 
       <Navbar />
 
