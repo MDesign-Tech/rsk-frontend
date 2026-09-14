acdd5031b718c916eaa41b29403b3927d8ac1ff1
Mussa Nsabimana <nsamussa1@gmail.com>
2026-09-14 21:40:38 +0200

    Update favicont

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,12 +53,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "16x16", type: "image/svg+xml" },
-      { url: "/favicon.ico", sizes: "32x32", type: "image/svg+xml" },
-      { url: "/favicon.ico", sizes: "64x64", type: "image/svg+xml" },
+      { url: "/favicon.ico", sizes: "16x16" },
+      { url: "/favicon.ico", sizes: "32x32" },
+      { url: "/favicon.ico", sizes: "64x64" },
     ],
     shortcut: "/favicon.ico",
-    apple: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
   },
   manifest: "/manifest.json",
   metadataBase: new URL(
